aec3f0f300909a0fdabe0f4070a4c1e5917cbf0d
Owen Fielding <odf@email.sc.edu>
2020-02-18 07:44:32 -0500

    Assignment 6

---
+++ csce242/assignments/a6/script.js
@@ -0,0 +1,64 @@
+function display(){
+const firstName = document.getElementById("txt-name1").value;
+const firstAge = document.getElementById("txt-age1").value;
+
+const secondName = document.getElementById("txt-name2").value;
+const secondAge = document.getElementById("txt-age2").value;
+
+const thirdName = document.getElementById("txt-name3").value;
+const thirdAge = document.getElementById("txt-age3").value;
+
+let displayOld = document.getElementById("ex1-old");
+let displayYoung = document.getElementById("ex1-young");
+let displayMiddle = document.getElementById("ex1-middle");
+displayOld.innerHTML = `Oldest is ${getOldest(firstAge,secondAge,thirdAge)} years old`;
+displayMiddle.innerHTML=`Middle is ${getMiddle(firstAge,secondAge,thirdAge)} years old`;
+displayYoung.innerHTML= `Youngest is ${getYoung(firstAge,secondAge,thirdAge)} years old`;
+}
+function getOldest(age1,age2,age3){
+    if(age1 > age2 && age1 > age3){
+       return age1;
+    }
+    if(age2 > age1 && age2 > age3){
+        return age2;
+     }
+     if(age3 > age1 && age3 > age2){
+        return age3;
+     }
+    
+}
+function getMiddle(age1,age2,age3){
+    if(getYoung(age1,age2,age3) != age1 && getOldest(age1,age2,age3) != age1){
+        return age1;
+    }
+    if(getYoung(age1,age2,age3) != age2 && getOldest(age1,age2,age3) != age2){
+        return age2;
+    }
+        if(getYoung(age1,age2,age3) != age3 && getOldest(age1,age2,age3) != age3){ 
+            return age3;
+        }
+    }
+
+function getYoung(age1,age2,age3){
+    if(age1 < age2 && age1 < age3){
+       return age1;
+    }
+    if(age2 < age1 && age2 < age3){
+        return age2;
+     }
+     if(age3 < age1 && age3 < age2){
+        return age3;
+     }
+
+    
+}
+
+function toggleNav(){
+    let navItems = document.getElementById("main-nav-items");
+    navItems.classList.toggle("hidden");
+    
+}
+const navToggle = document.getElementById("nav-toggle");
+navToggle.onclick = toggleNav;
+const btnDisplay = document.getElementById("btn-display");
+btnDisplay.onclick = display;
